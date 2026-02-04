f9f3c874aa65012154f22456bf1e5e7719382097
kleva-j <kasmickleva@gmail.com>
2026-02-04 17:27:29 +0100

    chore: setup jest for testing

---
+++ apps/native/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'jest-expo',
+  testMatch: [
+    '**/__tests__/**/*.(ts|tsx|js)',
+    '**/*.(test|spec).(ts|tsx|js)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|effect)'
+  ]
+}
